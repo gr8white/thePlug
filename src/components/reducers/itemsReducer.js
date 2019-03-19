098ab2279e1c08f950afbd19951d5ffec54d40e3
Derrick White <gr8white@uga.edu>
2019-03-19 15:30:56 -0500

    added new items

--- src/components/reducers/itemsReducer.js
+++ src/components/reducers/itemsReducer.js
@@ -1,84 +1,111 @@
-import Item1 from '../images/sneaker-images/adidasSuperstar.jpg'
-import Item2 from '../images/sneaker-images/adidasUndefeated.jpg'
-import Item3 from '../images/sneaker-images/chuckTaylorCDG.jpg'
-import Item4 from '../images/sneaker-images/jordan1Bred.jpg'
-import Item5 from '../images/sneaker-images/jordan1OffWhite.jpg'
-import Item6 from '../images/sneaker-images/jordan1Royal.jpg'
-import Item7 from '../images/sneaker-images/jordan3BlackCement.jpg'
-import Item8 from '../images/sneaker-images/jordan3WhiteCement.jpg'
-import Item9 from '../images/sneaker-images/jordan4WhiteCement.jpg'
-import Item10 from '../images/sneaker-images/jordan11Bred.jpg'
-import Item11 from '../images/sneaker-images/nikeAdapt.jpg'
-import Item12 from '../images/sneaker-images/nikeAirForce1.jpg'
-import Item13 from '../images/sneaker-images/nikeAirMax1.jpg'
-import Item14 from '../images/sneaker-images/nikeEpicReact.jpg'
-import Item15 from '../images/sneaker-images/nikeFlyknitRacer.jpg'
-import Item16 from '../images/sneaker-images/nikeFlyknitTrainer.jpg'
-import Item17 from '../images/sneaker-images/nikeFOGAir.jpg'
-import Item18 from '../images/sneaker-images/nikeFOGSkylon.jpg'
-import Item19 from '../images/sneaker-images/nikeGalaxyFoamposite.jpg'
-import Item20 from '../images/sneaker-images/nikePresto.jpg'
-import Item21 from '../images/sneaker-images/vansFOGEra.jpg'
-import Item22 from '../images/sneaker-images/vansOldSkool.jpg'
-import Item23 from '../images/sneaker-images/vansYachtClub.jpg'
-import Item24 from '../images/sneaker-images/yeezyStatic.jpg'
-import Item25 from '../images/sneaker-images/nikeDunkPigeon.jpg'
-// import Item26 from '../images/sneaker-images/.jpg'
-// import Item27 from '../images/sneaker-images/.jpg'
-// import Item27 from '../images/sneaker-images/.jpg'
-// import Item28 from '../images/sneaker-images/.jpg'
-// import Item29 from '../images/sneaker-images/.jpg'
-// import Item30 from '../images/sneaker-images/.jpg'
-// import Item31 from '../images/sneaker-images/.jpg'
-// import Item32 from '../images/sneaker-images/.jpg'
-// import Item33 from '../images/sneaker-images/.jpg'
-// import Item34 from '../images/sneaker-images/.jpg'
-// import Item35 from '../images/sneaker-images/.jpg'
-// import Item36 from '../images/sneaker-images/.jpg'
-// import Item37 from '../images/sneaker-images/.jpg'
-// import Item38 from '../images/sneaker-images/.jpg'
-// import Item39 from '../images/sneaker-images/.jpg'
-// import Item40 from '../images/sneaker-images/.jpg'
-// import Item41 from '../images/sneaker-images/.jpg'
-// import Item42 from '../images/sneaker-images/.jpg'
-// import Item43 from '../images/sneaker-images/.jpg'
-// import Item44 from '../images/sneaker-images/.jpg'
-// import Item45 from '../images/sneaker-images/.jpg'
-// import Item46 from '../images/sneaker-images/.jpg'
-// import Item47 from '../images/sneaker-images/.jpg'
-// import Item48 from '../images/sneaker-images/.jpg'
-// import Item49 from '../images/sneaker-images/.jpg'
-// import Item50 from '../images/sneaker-images/.jpg'
+import Item1 from '../images/sneaker-images/adidasAWang.jpg'
+import Item2 from '../images/sneaker-images/adidasNeighborhood.jpg'
+import Item3 from '../images/sneaker-images/adidasNMDHumanRaceYellow.jpg'
+import Item4 from '../images/sneaker-images/adidasNMDNERD.jpg'
+import Item5 from '../images/sneaker-images/adidasUndefeated.jpg'
+import Item6 from '../images/sneaker-images/chuckTaylorCDG.jpg'
+import Item7 from '../images/sneaker-images/chuckTaylorKith .jpg'
+import Item8 from '../images/sneaker-images/jordan1Bred.jpg'
+import Item9 from '../images/sneaker-images/jordan1OffWhite.jpg'
+import Item10 from '../images/sneaker-images/jordan1OffWhiteUNC.jpg'
+import Item11 from '../images/sneaker-images/jordan1Royal.jpg'
+import Item12 from '../images/sneaker-images/jordan1Union.jpg'
+import Item13 from '../images/sneaker-images/jordan3BlackCement.jpg'
+import Item14 from '../images/sneaker-images/jordan3WhiteCement.jpg'
+import Item15 from '../images/sneaker-images/jordan4WhiteCement.jpg'
+import Item16 from '../images/sneaker-images/jordan11Bred.jpg'
+import Item17 from '../images/sneaker-images/nikeAdapt.jpg'
+import Item18 from '../images/sneaker-images/nikeAirForce1.jpg'
+import Item19 from '../images/sneaker-images/nikeAirForce1TravisScott.jpg'
+import Item20 from '../images/sneaker-images/nikeAirMag.jpg'
+import Item21 from '../images/sneaker-images/nikeAirMax1:97.jpg'
+import Item22 from '../images/sneaker-images/nikeAirMax1.jpg'
+import Item23 from '../images/sneaker-images/nikeAirMax1Parra.jpg'
+import Item24 from '../images/sneaker-images/nikeAirMax98Gundam.jpg'
+import Item25 from '../images/sneaker-images/nikeAirMax180CDG.jpg'
+import Item26 from '../images/sneaker-images/nikeAirMax720.jpg'
+import Item27 from '../images/sneaker-images/nikeAirPrestoAcronym.jpg'
+import Item28 from '../images/sneaker-images/nikeDunkDiamond.jpg'
+import Item29 from '../images/sneaker-images/nikeDunkPigeon.jpg'
+import Item30 from '../images/sneaker-images/nikeEpicReact.jpg'
+import Item31 from '../images/sneaker-images/nikeFlyknitRacer.jpg'
+import Item32 from '../images/sneaker-images/nikeFlyknitTrainer.jpg'
+import Item33 from '../images/sneaker-images/nikeFOGAir.jpg'
+import Item34 from '../images/sneaker-images/nikeFOGSkylon.jpg'
+import Item35 from '../images/sneaker-images/nikeGalaxyFoamposite.jpg'
+import Item36 from '../images/sneaker-images/nikeSupremeFoamposite.jpg'
+import Item37 from '../images/sneaker-images/nikeHyperAdapt.jpg'
+import Item38 from '../images/sneaker-images/nikeLebron15Kith.jpg'
+import Item39 from '../images/sneaker-images/nikePresto.jpg'
+import Item40 from '../images/sneaker-images/nikeReactElement87.jpg'
+import Item41 from '../images/sneaker-images/nikeTomSachsMarsYard.jpg'
+import Item42 from '../images/sneaker-images/nikeVaporMaxCDG.jpg'
+import Item43 from '../images/sneaker-images/nikeZoomFlyOffWhite.jpg'
+import Item44 from '../images/sneaker-images/vansFOGEra.jpg'
+import Item45 from '../images/sneaker-images/vansOldSkool.jpg'
+import Item46 from '../images/sneaker-images/vansYachtClub.jpg'
+import Item47 from '../images/sneaker-images/versaceChainReaction.jpg'
+import Item48 from '../images/sneaker-images/yeezy700.jpg'
+import Item49 from '../images/sneaker-images/yeezyStatic.jpg'
+import Item50 from '../images/sneaker-images/yeezyBoost750OG.jpg'
 
 const itemsReducer = {
   items: [
-    // {id:1,title:'adidasSuperstar', price:110,img:Item1},
-    {id:2,title:'Adidas Boost - Undefeated', price:800,img:Item2},
-    {id:3,title:'Chuck Taylor - Comme Des Garcon', price:120,img:Item3},
-    {id:4,title:'Jordan 1 - Bred', price:260,img:Item4},
-    {id:5,title:'Jordan 1 - Off White', price:160,img:Item5},
-    {id:6,title:'Jordan 1 - Royal', price:900,img:Item6},
-    {id:7,title:'Jordan 3 - Black Cement', price:110,img:Item7},
-    {id:8,title:'Jordan 3 - White Cement', price:800,img:Item8},
-    {id:9,title:'Jordan 4 - White Cement', price:120,img:Item9},
-    {id:10,title:'Jordan 11 - Bred', price:260,img:Item10},
-    {id:11,title:'Nike Adapt', price:160,img:Item11},
-    {id:12,title:'Nike Air Force 1', price:900,img:Item12},
-    {id:13,title:'Nike Air Max 1 - OG Red', price:110,img:Item13},
-    {id:14,title:'Nike Epic React', price:800,img:Item14},
-    {id:15,title:'Nike Flyknit Racer', price:120,img:Item15},
-    {id:16,title:'Nike Flyknit Trainer', price:260,img:Item16},
-    {id:17,title:'Nike Fear Of God - Air', price:160,img:Item17},
-    {id:18,title:'Nike Fear Of God - Skylon', price:900,img:Item18},
-    {id:19,title:'Nike Foamposite - Galaxy', price:110,img:Item19},
-    {id:20,title:'Nike Presto - Off White', price:800,img:Item20},
-    {id:21,title:'Vans Era - Fear Of God', price:120,img:Item21},
-    {id:22,title:'Vans Old Skool - OG', price:260,img:Item22},
-    {id:23,title:'Vans Old Skool - Yacht Club', price:160,img:Item23},
-    {id:24,title:'Yeezy 700 - Static', price:900,img:Item24},
-    {id:25,title:'Nike Dunk - Pigeon', price:900,img:Item25}
+    {id:1,title:'Adidas Alexander Wang', price:200, img:Item1},
+    {id:2,title:'Adidas Neighborhood', price:200, img:Item2},
+    {id:3,title:'Adidas NMD Human Race - Yellow', price:200, img:Item3},
+    {id:4,title:'Adidas NMD Human Race - N.E.R.D.', price:200, img:Item4},
+    {id:5,title:'Adidas Undefeated', price:200, img:Item5},
+    {id:6,title:'Chuck Taylor CDG', price:200, img:Item6},
+    {id:7,title:'Chuck Taylor Kith ', price:200, img:Item7},
+    {id:8,title:'Jordan 1 - Bred', price:200, img:Item8},
+    {id:9,title:'Jordan 1 - Off-White', price:200, img:Item9},
+    {id:10,title:'Jordan 1 - Off-White (UNC)', price:200, img:Item10},
+    {id:11,title:'Jordan 1 - Royal', price:200, img:Item11},
+    {id:12,title:'Jordan 1 - Union', price:200, img:Item12},
+    {id:13,title:'Jordan 3 - Black Cement', price:200, img:Item13},
+    {id:14,title:'Jordan 3 - 88', price:200, img:Item14},
+    {id:15,title:'Jordan 4 White Cement', price:200, img:Item15},
+    {id:16,title:'Jordan 11 Bred', price:200, img:Item16},
+    {id:17,title:'Nike Adapt', price:200, img:Item17},
+    {id:18,title:'Nike Air Force 1', price:200, img:Item18},
+    {id:19,title:'Nike Air Force 1 - Travis Scott', price:200, img:Item19},
+    {id:20,title:'Nike Air Mag', price:200, img:Item20},
+    {id:21,title:'Nike Air Max 1/97 - Sean Wotherspoon', price:200, img:Item21},
+    {id:22,title:'Nike Air Max 1', price:200, img:Item22},
+    {id:23,title:'Nike Air Max 1 - Parra', price:200, img:Item23},
+    {id:24,title:'Nike Air Max 98 - Gundam', price:200, img:Item24},
+    {id:25,title:'Nike Air Max 180 - CDG', price:200, img:Item25},
+    {id:26,title:'Nike Air Max 720', price:200, img:Item26},
+    {id:27,title:'Nike Air Presto - Acronym', price:200, img:Item27},
+    {id:28,title:'Nike Dunk - Diamond', price:200, img:Item28},
+    {id:29,title:'Nike Dunk - Pigeon', price:200, img:Item29},
+    {id:30,title:'Nike Epic React', price:200, img:Item30},
+    {id:31,title:'Nike Flyknit Racer', price:200, img:Item31},
+    {id:32,title:'Nike Flyknit Trainer', price:200, img:Item32},
+    {id:33,title:'Nike Air Fear of God', price:200, img:Item33},
+    {id:34,title:'Nike Fear of God Skylon', price:200, img:Item34},
+    {id:35,title:'Nike Foamposite - Galaxy', price:200, img:Item35},
+    {id:36,title:'Nike Foamposite - Supreme', price:200, img:Item40},
+    {id:37,title:'Nike Hyper Adapt', price:200, img:Item36},
+    {id:38,title:'Nike Lebron 15 - Kith', price:200, img:Item37},
+    {id:39,title:'Nike Presto', price:200, img:Item38},
+    {id:40,title:'Nike React Element 87', price:200, img:Item39},
+    {id:41,title:'Nike Mars Yard - Tom Sachs', price:200, img:Item41},
+    {id:42,title:'Nike Vapor Max - CDG', price:200, img:Item42},
+    {id:43,title:'Nike Zoom Fly - Off-White', price:200, img:Item43},
+    {id:44,title:'Vans Era - Fear of God', price:200, img:Item44},
+    {id:45,title:'Vans Old Skool', price:200, img:Item45},
+    {id:46,title:'Vans Old Skool - YachtClub', price:200, img:Item46},
+    {id:47,title:'Versace Chain Reaction', price:200, img:Item47},
+    {id:48,title:'Yeezy 700', price:200, img:Item48},
+    {id:49,title:'Yeezy 700 - Static', price:200, img:Item50},
+    {id:50,title:'Yeezy Boost 750', price:200, img:Item49},
   ],
   addRemovePhrase: ""
 } 
 
 export default itemsReducer
+
+
+
