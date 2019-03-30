a424069bb18de15aa36b45e2b6ecf166756a8883
Derrick White <gr8white@uga.edu>
2019-03-30 14:10:41 -0500

    Updates

---
+++ precache-manifest.ef7a8adc76c2401f9de78eb0bac31d60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "70bf3237e97e44d30d4228001e1f481e",
+    "url": "/thePlug/static/media/2w4o22.70bf3237.gif"
+  },
+  {
+    "revision": "a63479e9281c4f2e084f",
+    "url": "/thePlug/static/js/runtime~main.a63479e9.js"
+  },
+  {
+    "revision": "5e11f40066c60130bd3f",
+    "url": "/thePlug/static/js/main.5e11f400.chunk.js"
+  },
+  {
+    "revision": "b7fc2540b5a5659793cf",
+    "url": "/thePlug/static/js/2.b7fc2540.chunk.js"
+  },
+  {
+    "revision": "5e11f40066c60130bd3f",
+    "url": "/thePlug/static/css/main.8d1f91ea.chunk.css"
+  },
+  {
+    "revision": "6ed17f955dce86d559e8babdd2c9225f",
+    "url": "/thePlug/index.html"
+  }
+];
